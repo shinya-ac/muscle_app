0189f761b5e42c06f38ab3eb9ca34f0c9d165cb8
Ubuntu <ubuntu@ip-172-31-15-90.ap-northeast-1.compute.internal>
2020-12-29 05:55:03 +0000

    サインナップ機能追加完了

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rails',      '6.0.3'
+gem 'rails-i18n'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'bcrypt' ,    '3.1.13'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence:true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
     has_secure_password
-    validates :password, presence: true, length: {maximum: 6} 
+    validates :password, presence: true, length: {minimum: 6} 
 end
